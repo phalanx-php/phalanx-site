a94479188538f156b542c6b6f1c0092d14700216
Jonathan Havens <mail@jhavens.tech>
2026-06-06 13:42:56 -0700

    Update site bootstrap

--- bin/server.php
+++ bin/server.php
@@ -3,9 +3,9 @@
 
 declare(strict_types=1);
 
-use Phalanx\Http\Http;
-use Phalanx\Http\HttpApplication;
+use Phalanx\Http\Application;
 use Phalanx\Http\RouteGroup;
+use Phalanx\Http\Server;
 use Phalanx\Site\Handler\HealthCheck;
 use Phalanx\Site\Handler\HomePage;
 use Phalanx\Site\Handler\StaticFile;
@@ -13,8 +13,8 @@ use Phalanx\Site\PhalanxSiteBundle;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-return static function (array $context): HttpApplication {
-    return Http::starting($context)
+return static function (array $context): Application {
+    return Server::starting($context)
         ->providers(new PhalanxSiteBundle())
         ->routes(RouteGroup::of([
             'GET /' => HomePage::class,
