d372bac041b99fd841430d8b7d075c382dff3a73
Jonathan Havens <mail@jhavens.tech>
2026-06-08 01:54:38 -0700

    Add Pages workflow

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
+  base: process.env.PHALANX_SITE_BASE ?? '/',
   plugins: [
     TanStackRouterVite({
       target: 'react',
